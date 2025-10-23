0c8244a2e18a6ea89b654fb8cf38c565c490d8e5
Ahmad Danun Hanani <ahmadanun1@gmail.com>
2025-10-23 12:51:46 +0700

    use gemini instead

--- blog_generator.py
+++ blog_generator.py
@@ -1,26 +1,29 @@
-import openai
+from google import genai
+from google.genai import types
 from dotenv import dotenv_values
 
-config = dotenv_values(".env")
-openai.api_key = config['API_KEY']
+config = dotenv_values('.env')
 
-def generate_blog(paragraph_topic: str) -> str:
-  response = openai.completions.create(
-    model = 'gpt-3.5-turbo-instruct',
-    prompt = 'Write a paragraph about the following topic. ' + paragraph_topic,
-    max_tokens = 400,
-    temperature = 0.3
+client = genai.Client()
+
+def generate_blog(paragraph_topic: str) -> str | None:
+  response = client.models.generate_content( # type: ignore
+    model="gemini-2.5-flash",
+    contents=f'Write a detailed blog paragraph about {paragraph_topic}.',
+    config=types.GenerateContentConfig(
+      temperature=0.3,
+    )
   )
 
-  retrieve_blog = response.choices[0].text
-
+  retrieve_blog = response.text
   return retrieve_blog
 
 keep_writing = True
+
 while keep_writing:
-  answer = input('Write a paragraph? Y for yes, anything else for no: ')
+  answer = input('Write a paragraph? Y for yes, anything else for no. ')
   if (answer == 'Y'):
-    paragraph_topic = input('What should this paragraph talk about?')
+    paragraph_topic = input('What should this paragraph talk about? ')
     print(generate_blog(paragraph_topic))
   else:
     keep_writing = False
